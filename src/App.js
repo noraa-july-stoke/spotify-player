72848001da47c03f33970a28701e6295cd93ef95
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-03-28 00:44:08 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+//==================================================
+//  ╔═╗┌─┐┌─┐  ╦┌─┐
+//  ╠═╣├─┘├─┘  ║└─┐
+//  ╩ ╩┴  ┴  o╚╝└─┘
+//==================================================
+import { useState, useEffect } from "react";
+import Player from "./Player";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "YOUR_CLIENT_ID_GOES_HERE";
+const redirectUri = "http://localhost:3000";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
+
+const App = () => {
+ const [token, setToken] = useState(null);
+ const [item, setItem] = useState({
+   album: {
+     images: [{ url: "" }],
+   },
+   name: "",
+   artists: [{ name: "" }],
+   duration_ms: 0,
+ });
+ const [isPlaying, setIsPlaying] = useState("Paused");
+ const [progressMs, setProgressMs] = useState(0);
+
+ async function getCurrentlyPlaying(token) {
+   try {
+     const response = await fetch("https://api.spotify.com/v1/me/player", {
+       headers: {
+         Authorization: `Bearer ${token}`,
+       },
+     });
+
+     const data = await response.json();
+     setItem(data.item);
+     setIsPlaying(data.is_playing);
+     setProgressMs(data.progress_ms);
+   } catch (error) {
+     console.log(error);
+   }
+ }
+
+ useEffect(() => {
+   // Get the hash of the url
+   const hash = window.location.hash
+     .substring(1)
+     .split("&")
+     .reduce(function (initial, item) {
+       if (item) {
+         var parts = item.split("=");
+         initial[parts[0]] = decodeURIComponent(parts[1]);
+       }
+       return initial;
+     }, {});
+   window.location.hash = "";
+
+   // Set token
+   if (hash.access_token) {
+     setToken(hash.access_token);
+   }
+ }, []);
+
+ useEffect(() => {
+   if (token) {
+     getCurrentlyPlaying(token);
+   }
+ }, [token]);
+
+ return (
+   <div className="App">
+     <header className="App-header">
+       <img src={logo} className="App-logo" alt="logo" />
+       {!token && (
+         <a
+           className="btn btn--loginApp-link"
+           href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+             "%20"
+           )}&response_type=token&show_dialog=true`}>
+           Login to Spotify
+         </a>
+       )}
+       {token && (
+         <Player item={item} isPlaying={isPlaying} progressMs={progressMs} />
+       )}
+     </header>
+   </div>
+ );
 }
 
 export default App;
---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,66 @@
+//=======================================================================
+//  ╔═╗┬  ┌─┐┬ ┬┌─┐┬─┐
+//  ╠═╝│  ├─┤└┬┘├┤ ├┬┘
+//  ╩  ┴─┘┴ ┴ ┴ └─┘┴└─
+//======================================================================
+//       __  __  ___        __  __          ___         __  __  _______
+//  |\ |/  \|  \|__    |\/|/  \|  \|  ||   |__    ||\/||__)/  \|__)|/__`
+//  | \|\__/|__/|___   |  |\__/|__/\__/|___|___   ||  ||   \__/|  \|.__/
+//=======================================================================
+import React from "react";
+//=======================================================================
+//       __  __             ___     ___         __  __  _______
+//  |   /  \/  ` /\ |      |__||   |__    ||\/||__)/  \|__)|/__`
+//  |___\__/\__,/~~\|___   |  ||___|___   ||  ||   \__/|  \|.__/
+//=======================================================================
+// COMPONENTS
+// HELPERS
+// CONTEXTS
+// STYLES
+import "./Player.css";
+//=======================================================================
+const Player = (props) => {
+  //=================================================
+  //   VARIABLE DECLARATIONS, INITIALIZERS,
+  //   STATE VARIABLE ASSIGNMENTS, INLINE STYLE OBJ.
+  //=================================================
+  const backgroundStyles = {
+    backgroundImage: `url(${props.item.album.images[0].url})`,
+  };
+  const progressBarStyles = {
+    width: (props.progress_ms * 100) / props.item.duration_ms + "%",
+  };
+  //====================================
+  //              HOOKS
+  //====================================
+  //====================================
+  //      HELPERS/EVENT LISTENERS
+  //         ADDITIONAL LOGIC
+  //====================================
+  //====================================
+  //            JSX BODY
+  //====================================
+  return (
+    <div className="App">
+      <div className="main-wrapper">
+        <div className="now-playing__img">
+          <img src={props.item.album.images[0].url} />
+        </div>
+        <div className="now-playing__side">
+          <div className="now-playing__name">{props.item.name}</div>
+          <div className="now-playing__artist">
+            {props.item.artists[0].name}
+          </div>
+          <div className="now-playing__status">
+            {props.is_playing ? "Playing" : "Paused"}
+          </div>
+          <div className="progress">
+            <div className="progress__bar" style={progressBarStyles} />
+          </div>
+        </div>
+        <div className="background" style={backgroundStyles} />{" "}
+      </div>
+    </div>
+  );
+};
+export default Player;
