afb551beccf0bb42f4eeff393f055c6ec9a71f60
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-03-28 00:59:07 -0400

    basic auth/display status done

--- src/App.js
+++ src/App.js
@@ -4,94 +4,91 @@
 //  ╩ ╩┴  ┴  o╚╝└─┘
 //==================================================
 import { useState, useEffect } from "react";
-import Player from "./Player";
+import Player from "./components/Player";
 import logo from "./logo.svg";
 import "./App.css";
 
-export const authEndpoint = 'https://accounts.spotify.com/authorize';
+export const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
-const clientId = "YOUR_CLIENT_ID_GOES_HERE";
+const clientId = "47d6565a03d645a291eb1a093f48e121";
 const redirectUri = "http://localhost:3000";
-const scopes = [
-  "user-read-currently-playing",
-  "user-read-playback-state",
-];
+const scopes = ["user-read-currently-playing", "user-read-playback-state"];
 
 const App = () => {
- const [token, setToken] = useState(null);
- const [item, setItem] = useState({
-   album: {
-     images: [{ url: "" }],
-   },
-   name: "",
-   artists: [{ name: "" }],
-   duration_ms: 0,
- });
- const [isPlaying, setIsPlaying] = useState("Paused");
- const [progressMs, setProgressMs] = useState(0);
+  const [token, setToken] = useState(null);
+  const [item, setItem] = useState({
+    album: {
+      images: [{ url: "" }],
+    },
+    name: "",
+    artists: [{ name: "" }],
+    duration_ms: 0,
+  });
+  const [isPlaying, setIsPlaying] = useState("Paused");
+  const [progressMs, setProgressMs] = useState(0);
 
- async function getCurrentlyPlaying(token) {
-   try {
-     const response = await fetch("https://api.spotify.com/v1/me/player", {
-       headers: {
-         Authorization: `Bearer ${token}`,
-       },
-     });
+  async function getCurrentlyPlaying(token) {
+    try {
+      const response = await fetch("https://api.spotify.com/v1/me/player", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
-     const data = await response.json();
-     setItem(data.item);
-     setIsPlaying(data.is_playing);
-     setProgressMs(data.progress_ms);
-   } catch (error) {
-     console.log(error);
-   }
- }
+      const data = await response.json();
+      setItem(data.item);
+      setIsPlaying(data.is_playing);
+      setProgressMs(data.progress_ms);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
- useEffect(() => {
-   // Get the hash of the url
-   const hash = window.location.hash
-     .substring(1)
-     .split("&")
-     .reduce(function (initial, item) {
-       if (item) {
-         var parts = item.split("=");
-         initial[parts[0]] = decodeURIComponent(parts[1]);
-       }
-       return initial;
-     }, {});
-   window.location.hash = "";
+  useEffect(() => {
+    // Get the hash of the url
+    const hash = window.location.hash
+      .substring(1)
+      .split("&")
+      .reduce(function (initial, item) {
+        if (item) {
+          var parts = item.split("=");
+          initial[parts[0]] = decodeURIComponent(parts[1]);
+        }
+        return initial;
+      }, {});
+    window.location.hash = "";
 
-   // Set token
-   if (hash.access_token) {
-     setToken(hash.access_token);
-   }
- }, []);
+    // Set token
+    if (hash.access_token) {
+      setToken(hash.access_token);
+    }
+  }, []);
 
- useEffect(() => {
-   if (token) {
-     getCurrentlyPlaying(token);
-   }
- }, [token]);
+  useEffect(() => {
+    if (token) {
+      getCurrentlyPlaying(token);
+    }
+  }, [token]);
 
- return (
-   <div className="App">
-     <header className="App-header">
-       <img src={logo} className="App-logo" alt="logo" />
-       {!token && (
-         <a
-           className="btn btn--loginApp-link"
-           href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-             "%20"
-           )}&response_type=token&show_dialog=true`}>
-           Login to Spotify
-         </a>
-       )}
-       {token && (
-         <Player item={item} isPlaying={isPlaying} progressMs={progressMs} />
-       )}
-     </header>
-   </div>
- );
-}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="" />
+        {!token && (
+          <a
+            className="btn btn--loginApp-link"
+            href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+              "%20"
+            )}&response_type=token&show_dialog=true`}>
+            Login to Spotify
+          </a>
+        )}
+        {token && (
+          <Player item={item} isPlaying={isPlaying} progressMs={progressMs} />
+        )}
+      </header>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -44,7 +44,7 @@ const Player = (props) => {
     <div className="App">
       <div className="main-wrapper">
         <div className="now-playing__img">
-          <img src={props.item.album.images[0].url} />
+          <img src={props.item.album.images[0].url} alt="" />
         </div>
         <div className="now-playing__side">
           <div className="now-playing__name">{props.item.name}</div>
